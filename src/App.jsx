d477e883204c0ffd8bb045e8dfd9215bbb5bd18c
sreenath k k <sreenathkk00@gmail.com>
2025-03-06 15:01:44 +0530

    Initalized

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Initalized Sreerag Unni Portfolio </h1>
+    </div>
+  );
+}
